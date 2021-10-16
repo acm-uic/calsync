631355f0dab2898bd89c8f0653948ae821ced215
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2021-10-15 17:21:41 -0700

    :tada: repo setup

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import('prettier').Options} */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 120,
+  semi: true,
+  singleQuote: false,
+};
+
+module.exports = config;
