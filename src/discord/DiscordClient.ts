9afcd779428f3b0b4a20990830a7a173cc9bada9
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2021-10-23 17:15:28 -0700

    🌟 Update / delete discord events + fixes (#2)
    * discord events crud
    
    * add getchannels
    
    * update jsdoc
    
    * add channel name
    
    * fix typo

--- src/discord/DiscordClient.ts
+++ src/discord/DiscordClient.ts
@@ -1,73 +1,31 @@
 /**
- * @file This file exposes a discord client that can query and create events from a discord server.
+ * @file This file exposes a Discord HTTP API Client.
  */
 
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 import { DISCORD_API_BASE, DISCORD_SUPER_PROPERTIES } from "../config";
 import logger from "../util/logger";
 
-export enum DiscordEventEntityType {
+export enum EventEntityType {
+  STAGE_CHANNEL = 1,
   VOICE_CHANNEL = 2,
   SOMEWHERE_ELSE = 3,
 }
 
-export enum DiscordEventStatus {
+export enum EventStatus {
   UPCOMING = 1,
   ONGOING = 2,
 }
 
-export interface IDiscordClientConfig {
-  // TODO: add bot permissions to jsdoc. Permissions are currently unknown as the events api is not documented. It works with Administrator permissions.
-  /** Discord bot token. */
-  botToken: string;
+export interface IDiscordClientParams {
+  /**
+   * Discord auth header with appropriate permissions.
+   * Bot headers look like: `Bot TOKEN_GOES_HERE`
+   */
+  authHeader: string;
   /** Guild / Server ID */
   guildId: string;
 }
-
-export interface IDiscordEvent {
-  /** Unique event id. */
-  id: string;
-  /** Discord guild/server associated with the event. */
-  guild_id: string;
-  /** Voice channel id. Null when {@link entity_type} is VOICE_CHANNEL. */
-  channel_id: string | null;
-  /** Event name. */
-  name: string;
-  /** Event description. Null when description is empty. */
-  description: string | null;
-  /** TDB. Always seems to be null. */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  image: any | null;
-  /** Event start time in ISO Format. */
-  scheduled_start_time: string;
-  /** Event end time in ISO Format. Null when {@link entity_type} is VOICE_CHANNEL. */
-  scheduled_end_time: string | null;
-  /** TBD. Seems to always be 2. */
-  privacy_level: number;
-  /** Discord event status. {@link DiscordEventStatus} */
-  status: DiscordEventStatus;
-  /** Discord event type. {@link DiscordEventEntityType} */
-  entity_type: DiscordEventEntityType;
-  /** TBD. Seems to always be null. */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  entity_id: any | null;
-  /** Discord event metadata. Describes event location when {@link entity_type} is SOMEWHERE_ELSE, null otherwise. */
-  entity_metadata: {
-    location: string;
-  } | null;
-  /** TDB. always seems to be an empty array. */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  sku_ids: any[];
-  /** TDB. always seems to be an empty array. */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  skus: any[];
-}
-export interface IDiscordEventWithUserCount extends IDiscordEvent {
-  /** Available when passing with_user_count query string parameter. */
-  user_count: number;
-}
-
-export type IDiscordCreateEventResponse = Omit<IDiscordEvent, "skus">;
 
 export interface ICreateEventRequestData {
   /** Event name. */
@@ -78,29 +36,97 @@ export interface ICreateEventRequestData {
   privacy_level: number;
   /** Event start time in ISO Format. */
   scheduled_start_time: string;
-  /** Event end time in ISO Format. Null when {@link entity_type} is VOICE_CHANNEL. */
-  scheduled_end_time: string;
-  /** Discord event type. {@link DiscordEventEntityType} */
-  entity_type: DiscordEventEntityType;
-  /** Voice channel id. Null when event_type is not 2 (voice channel) */
+  /** Event end time in ISO Format. Null when {@link entity_type} is {@link EventEntityType.VOICE_CHANNEL}. */
+  scheduled_end_time?: string | null;
+  /** Discord event type. {@link EventEntityType} */
+  entity_type: EventEntityType;
+  /** Voice channel id. Null when {@link entity_type} is {@link EventEntityType.SOMEWHERE_ELSE} */
   channel_id: string | null;
-  /** Discord event metadata. Describes event location when {@link entity_type} is SOMEWHERE_ELSE, null otherwise. */
-  entity_metadata: {
-    location: string;
+  /** Discord event metadata. */
+  entity_metadata?: {
+    /** Event location, present when {@link entity_type} is {@link EventEntityType.SOMEWHERE_ELSE} */
+    location?: string;
+    /** , present when {@link entity_type} is {@link EventEntityType.STAGE_CHANNEL} */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    speaker_ids?: any[];
   } | null;
 }
+
+export interface IEvent extends ICreateEventRequestData {
+  /** Unique event id. */
+  id: string;
+  /** Discord guild/server associated with the event. */
+  guild_id: string;
+  /** TDB. Always seems to be null. */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  image: any | null;
+  /** Discord event status. {@link EventStatus} */
+  status: EventStatus;
+  /** TBD. Seems to always be null. */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  entity_id: any | null;
+  /** Discord event metadata. */
+  /** TDB. always seems to be an empty array. */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  sku_ids: any[];
+  /** TDB. always seems to be an empty array. */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  skus: any[];
+}
+export interface IEventWithUserCount extends IEvent {
+  /** Available when passing with_user_count query string parameter. */
+  user_count: number;
+}
+
+export type ICreatePatchEventResponse = Omit<IEvent, "skus">;
+export type IDeleteEventResponse = Record<string, never>;
 
 export interface IGetEventsParams {
   withUserCount: boolean;
 }
 
+export interface ICreateEventParams {
+  eventData: ICreateEventRequestData;
+}
+
+export interface IDeleteEventParams {
+  id: string;
+}
+
+export interface IPatchEventParams extends ICreateEventParams {
+  id: string;
+}
+
+export enum ChannelType {
+  GUILD_VOICE = 2,
+  GUILD_STAGE_VOICE = 13,
+}
+
+export interface IBaseChannel {
+  id: string;
+  type: number;
+  name: string;
+}
+
+/**
+ * Discord Events API Client
+ *
+ * @remarks
+ * Discord events API is currently not publicly available, see {@link https://support.discord.com/hc/en-us/community/posts/4410436617879-Scheduled-Events-Bot-API}
+ *
+ * @public
+ */
 export class DiscordClient {
-  private _config: IDiscordClientConfig;
+  private _config: IDiscordClientParams;
   private _apiClient: AxiosInstance;
 
-  public constructor(config: IDiscordClientConfig) {
+  public constructor(config: IDiscordClientParams) {
     this._config = config;
-    this._apiClient = axios.create({ headers: { Authorization: `bot ${this._config.botToken}` } });
+    this._apiClient = axios.create({
+      baseURL: `${DISCORD_API_BASE}/v9`,
+    });
+    this._apiClient.defaults.headers.common["Authorization"] = this._config.authHeader;
+    this._apiClient.defaults.headers.common["x-super-properties"] = DISCORD_SUPER_PROPERTIES;
   }
 
   /**
@@ -111,21 +137,15 @@ export class DiscordClient {
    */
   public async getEvents<O extends IGetEventsParams>(
     params: O
-  ): Promise<O extends { withUserCount: true } ? IDiscordEventWithUserCount[] : IDiscordEvent[]> {
+  ): Promise<O extends { withUserCount: true } ? IEventWithUserCount[] : IEvent[]> {
     try {
       return (
-        await this._apiClient.get<O extends { withUserCount: true } ? IDiscordEventWithUserCount[] : IDiscordEvent[]>(
-          `${DISCORD_API_BASE}/v9/guilds/${this._config.guildId}/events?with_user_count=${params.withUserCount}`,
-          {
-            headers: {
-              // API does not seem to work without passing x-super-properties
-              "x-super-properties": DISCORD_SUPER_PROPERTIES,
-            },
-          }
+        await this._apiClient.get<O extends { withUserCount: true } ? IEventWithUserCount[] : IEvent[]>(
+          `/guilds/${this._config.guildId}/events?with_user_count=${params.withUserCount}`
         )
       ).data;
     } catch (e) {
-      logger.error(`Error getting events from discord. Error message: ${(e as Error).message}`);
+      logger.error(`Error getting events from discord. Response: ${JSON.stringify((e as AxiosError).response?.data)}`);
       throw e;
     }
   }
@@ -133,32 +153,88 @@ export class DiscordClient {
   /**
    * Create event in the discord server.
    * Note: this API is currently not available to bots.
-   * @see {@link https://support.discord.com/hc/en-us/community/posts/4410436617879-Scheduled-Events-Bot-API}
    * @throws Will throw error if HTTP request was not successful.
-   * @param eventData Event information
+   * @param params Event information
    * @returns Event data if successful
    */
-  public async createEvent(eventData: ICreateEventRequestData): Promise<IDiscordCreateEventResponse> {
+  public async createEvent(params: ICreateEventParams): Promise<ICreatePatchEventResponse> {
+    const { eventData } = params;
     try {
       return (
-        await this._apiClient.post<IDiscordCreateEventResponse>(
-          `${DISCORD_API_BASE}/v9/guilds/${this._config.guildId}/events?with_user_count=true`,
-          eventData,
-          {
-            headers: {
-              // API does not seem to work without passing x-super-properties
-              "x-super-properties": DISCORD_SUPER_PROPERTIES,
-              "Content-Type": "application/json",
-            },
-          }
-        )
+        await this._apiClient.post<ICreatePatchEventResponse>(`/guilds/${this._config.guildId}/events`, eventData, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
       ).data;
     } catch (e) {
       logger.error(
-        `Error creating event in discord. Event information ${JSON.stringify(eventData)}. Error message: ${
-          (e as Error).message
+        `Error creating event in discord. Event information ${JSON.stringify(eventData)}. Response: ${JSON.stringify(
+          (e as AxiosError).response?.data
+        )}`
+      );
+      throw e;
+    }
+  }
+
+  /**
+   * Delete event in the discord server.
+   * Note: this API is currently not available to bots.
+   * @throws Will throw error if HTTP request was not successful.
+   * @param params Event ID to delete
+   * @returns Blank response if successful
+   */
+  public async deleteEvent(params: IDeleteEventParams): Promise<IDeleteEventResponse> {
+    const { id } = params;
+    try {
+      return (await this._apiClient.delete<IDeleteEventResponse>(`/guild-events/${id}`)).data;
+    } catch (e) {
+      logger.error(
+        `Error deleting event in discord. Event id ${id}. Response: ${JSON.stringify((e as AxiosError).response?.data)}`
+      );
+      throw e;
+    }
+  }
+
+  /**
+   * Patch event in the discord server.
+   * Note: this API is currently not available to bots.
+   * @throws Will throw error if HTTP request was not successful.
+   * @param params Event ID to patch and updated event information
+   * @returns Event data if successful
+   */
+  public async patchEvent(params: IPatchEventParams): Promise<ICreatePatchEventResponse> {
+    const { id, eventData } = params;
+    try {
+      return (
+        await this._apiClient.patch<ICreatePatchEventResponse>(`/guild-events/${id}`, eventData, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+      ).data;
+    } catch (e) {
+      logger.error(
+        `Error patching event in discord. Event id: ${id}. Event information: ${JSON.stringify(eventData)}. Response: ${
+          (e as AxiosError).response?.data
         }`
       );
+      throw e;
+    }
+  }
+
+  /**
+   * Get channels on a Discord server.
+   * @throws Will throw error if HTTP request was not successful.
+   * @returns Array of pruned channels (id, type) if successful
+   */
+  public async getChannels(): Promise<IBaseChannel[]> {
+    try {
+      return (await this._apiClient.get<IBaseChannel[]>(`/guild/${this._config.guildId}/channels`)).data.map(
+        ({ id, type, name }): IBaseChannel => ({ id, type, name })
+      );
+    } catch (e) {
+      logger.error(`Error getting channels from discord. Response: ${(e as AxiosError).response?.data}`);
       throw e;
     }
   }
